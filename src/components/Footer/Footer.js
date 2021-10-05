812c4646355c3139e6c73f061c9bf505631d29bf
Zaki003 <zakifaiazchy.work@gmail.com>
2021-10-05 20:18:40 +0600

    header and footer done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import { Box } from '@mui/system';
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <Box className='footer'>
+            <h3>Edu-Zaki Private Limited, committed to give the best learning service.</h3>
+            <p><small>© 2021 Edu-Zaki Private Limited. All rights reserved</small></p>
+            <hr />
+        </Box>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import { Box } from '@mui/system';
+import React from 'react';
+import './Header.css';
+import logo from '../../images/logo.png';
+import { Grid, Item } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Box className='header'>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                <Box id='company'>
+                    <img src={logo} alt="" />
+                    <h2 id='company-name'>Edu-Zaki</h2>
+                </Box>
+                <Box className='links'>
+                    <nav className='menu'>
+                        <NavLink to='/home'>Home</NavLink>
+                        <NavLink to='/about'>About Us</NavLink>
+                        <NavLink to='/courses'>Courses</NavLink>
+                        <NavLink to='/pages'>Pages</NavLink>
+                    </nav>
+                </Box>
+
+            </Grid>
+
+        </Box>
+    );
+};
+
+export default Header;
