23b34d82095c882630e13ad4eaa77546ba4362ae
Zaki003 <zakifaiazchy.work@gmail.com>
2021-10-05 23:45:27 +0600

    about us page added

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useCourses from '../../hooks/useCourses';
+import './AboutUs.css';
 
 const AboutUs = () => {
+    const [courses] = useCourses();
     return (
-        <div>
-
+        <div className='about-us'>
+            <h1>Welcome to Edu-Zaki!</h1>
+            <h2>Welcome to a wonderful learning environment</h2>
+            <div>
+                <h3>Our Teachers: </h3>
+                {
+                    courses.map(course => <h4>{course.teacher}</h4>)
+                }
+            </div>
         </div>
     );
 };
